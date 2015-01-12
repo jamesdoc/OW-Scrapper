dc2f9a0c9ebf17d7d6169a6b439b3269de50e166
Doc <hi@jamesdoc.com>
2015-01-12 19:37:54 +0000

    Adding in placeholders and copyright notice

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+ #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import tornado.ioloop
 import tornado.web
 from tornado.options import define, options, parse_command_line
 import json
 import requests
 from bs4 import BeautifulSoup
-
+from datetime import date
 from test_soup import TEST_SOUP
 
 define("port", default=8080, help="run on the given port", type=int)
@@ -26,7 +29,14 @@ class IndexHandler(tornado.web.RequestHandler):
 
     	ow_dict = {
     		'country_name': str(ow_soup.title.get_text().replace(" | Operation World", "")),
-    		'url': url % country
+    		'url': url % country,
+    		'geography': {},
+    		'people': {},
+    		'religion': {},
+    		'prayer_answers': {},
+    		'prayer_challenges': {},
+    		'copyright': "All content copyright Operation World. For more information visit the OW website (operationworld.org). Data retrieved: %s." % date.today()
+
     	}
 
     	ow_json = json.dumps(ow_dict)
